1a59e1a3b64e8cdced776a81e459858ec98668e5
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-08-15 14:44:36 +0500

    fix responsiveness of setup

--- src/Pages/Setup.jsx
+++ src/Pages/Setup.jsx
@@ -11,7 +11,7 @@ import BoxSetup from '../Components/BoxSetup'
 import CastingSetup from '../Components/CastingSetup'
 import Slide from 'react-reveal/Slide';
 import { ButtonBase } from '@mui/material';
-
+import MediaQuery from 'react-responsive'
 
 
 
@@ -32,26 +32,27 @@ const Setup = ({ darkMode, setDarkMode }) => {
 
 
     return (
-        
+
         <div className=' w-screen p-8 md:p-0  mx-auto flex flex-col items-center justify-center'>
 
             <h1 className='text-center font-bold text-5xl  mt-20  text-white'>DOWNLOAD THE BEST IPTV APP</h1>
             <p className='text-center font-bold text-2xl mt-4  text-white'>Our Apps Work Flawlessly on tv, mobile and browser</p>
 
-            
-                <div className='mt-24  w-full md:w-[90%]  lg:max-w-[1100px] flex-wrap rounded-mg shadow-sm mx-auto flex items-center justify-between flex-col gap-8'>
-                    <div className='w-full h-full p-2 justify-between  flex-wrap md:flex-row flex-col flex  gap-8'>
 
+            <div className='mt-24  w-full md:w-[90%]  lg:max-w-[1100px] flex-wrap rounded-mg shadow-sm mx-auto flex items-center justify-between flex-col gap-8'>
+                <div className='w-full p-2 justify-between  flex-wrap md:flex-row flex-col flex  gap-8'>
+
+                    <MediaQuery minWidth={798} >
                         {/* /icon 1 */}
                         <div>
                             <ButtonBase style={{ height: 'max' }}>
-                                <div className='h-full' onClick={(e) => {
+                                <div className='h-full w-full' onClick={(e) => {
 
                                     WebRef.current?.scrollIntoView({ behavior: 'smooth' });
                                     setCurrent('Web');
                                     setPrevIndex(index);
                                     setIndex(0);
-                                    
+
 
                                 }}
 
@@ -108,7 +109,7 @@ const Setup = ({ darkMode, setDarkMode }) => {
                         <div>
                             <ButtonBase>
                                 <div onClick={(e) => {
-                                    
+
                                     setCurrent('Box & Stick')
                                     setPrevIndex(index);
                                     setIndex(3);
@@ -127,7 +128,7 @@ const Setup = ({ darkMode, setDarkMode }) => {
                         <div>
                             <ButtonBase>
                                 <div onClick={(e) => {
-                                    
+
                                     setCurrent('Casting');
                                     setPrevIndex(index);
                                     setIndex(4);
@@ -143,28 +144,139 @@ const Setup = ({ darkMode, setDarkMode }) => {
 
                             </ButtonBase>
                         </div>
-                    </div>
+                    </MediaQuery>
                 </div>
 
-           
-            
-            
-       
+            </div>
+
+            <MediaQuery minWidth={0} maxWidth={768}>
+
+                <div className='mt-24  w-full md:w-[90%]  lg:max-w-[1100px] flex-wrap rounded-mg shadow-sm mx-auto flex items-center justify-between flex-col gap-8'>
+                    <div className='w-full p-2 justify-between  flex-wrap md:flex-row flex-col flex  gap-8'>
+
+                        {/* /icon 1 */}
+
+                        <ButtonBase style={{ height: 'max' }}>
+                            <div className='h-full w-full' onClick={(e) => {
+
+                                WebRef.current?.scrollIntoView({ behavior: 'smooth' });
+                                setCurrent('Web');
+                                setPrevIndex(index);
+                                setIndex(0);
+
+
+                            }}
+
+                                className={` ${current == 'Web' ? Activebtn : notActiveBtn}`}>
+
+
+                                <button >
+                                    <TbWorld fontSize={90} />
+                                    <h1 className='font-bold text-xl'><a href="web">Web</a></h1>
+                                </button>
+
+
+                            </div>
+                        </ButtonBase>
+
+                        {/* icons 2 */}
+
+                        <ButtonBase>
+                            <div onClick={(e) => {
+                                MobileRef.current?.scrollIntoView({ behavior: 'smooth' });
+                                setPrevIndex(index);
+                                setCurrent('Mobile');
+                                setIndex(1);
+                            }} className={` ${current == 'Mobile' ? Activebtn : notActiveBtn}`}>
+
+                                <button>
+                                    <FaMobile fontSize={90} />
+                                    <h1 className='font-bold text-xl'>Mobile</h1>
+                                </button>
+
+                            </div>
+                        </ButtonBase>
+
+                        {/* icon 3 */}
+
+                        <ButtonBase>
+                            <div onClick={(e) => {
+                                setCurrent('TV')
+                                setPrevIndex(index);
+                                setIndex(2);
+                                TvRef.current?.scrollIntoView({ behavior: 'smooth' });
+
+                            }} className={` ${current == 'TV' ? Activebtn : notActiveBtn}`}>
+                                <button>
+                                    <FiMonitor fontSize={90} />
+                                    <h1 className='font-bold text-xl'>TV</h1>
+                                </button>
+
+                            </div>
+
+                        </ButtonBase>
+
+                        {/* icon 4 */}
+
+                        <ButtonBase>
+                            <div onClick={(e) => {
+
+                                setCurrent('Box & Stick')
+                                setPrevIndex(index);
+                                setIndex(3);
+                                BoxRef.current?.scrollIntoView({ behavior: 'smooth' });
+
+                            }} className={` ${current == 'Box & Stick' ? Activebtn : notActiveBtn}`}>
+                                <button>
+                                    <TbDevices fontSize={60} />
+                                    <h1 className='font-bold text-xl'>Box & Stick</h1>
+                                </button>
+
+                            </div>
+                        </ButtonBase>
+
+
+
+                        <ButtonBase>
+                            <div onClick={(e) => {
+
+                                setCurrent('Casting');
+                                setPrevIndex(index);
+                                setIndex(4);
+                                CastingRef.current?.scrollIntoView({ behavior: 'smooth' });
+
+                            }} className={` ${current == 'Casting' ? Activebtn : notActiveBtn}`}>
+                                <button>
+                                    <h1><MdCastConnected fontSize={90} /></h1>
+                                    <h1 className='font-bold text-xl'>Casting</h1>
+                                </button>
+
+                            </div>
+
+                        </ButtonBase>
+
+                    </div>
+                </div>
+            </MediaQuery>
+
+
+
+
             <div className='mt-6 max-w-full md:w-[90%]  lg:max-w-[1100px] dark:bg-blue-darkmd bg-white dark:text-white transition-all duration-300 ease-in-out text-blue-lightmd rounded-lg shadow-lg mx-auto flex items-center justify-center p-8'>
-                {current === 'Web' && (prevIndex >= index)? <Slide duration= {700} enter = 'false' left> <div ref={WebRef}><WebSetup /></div> </Slide > : ''}
-                {current === 'Web' && (prevIndex < index)? <Slide duration= {700} enter = 'false' right> <div ref={WebRef}><WebSetup /></div> </Slide > : ''}
+                {current === 'Web' && (prevIndex >= index) ? <Slide duration={700} enter='false' left> <div ref={WebRef}><WebSetup /></div> </Slide > : ''}
+                {current === 'Web' && (prevIndex < index) ? <Slide duration={700} enter='false' right> <div ref={WebRef}><WebSetup /></div> </Slide > : ''}
 
-                {current === 'Mobile'  && (prevIndex >= index) ? <Slide left duration= {700} enter = 'false'> <div ref={MobileRef}> <MobileSetup /></div> </Slide > : ''}
-                {current === 'Mobile'  && (prevIndex < index) ? <Slide right duration= {700} enter = 'false'> <div ref={MobileRef}> <MobileSetup /></div> </Slide > : ''}
-                
-                {current === 'TV' && (prevIndex >= index) ? <Slide left duration= {700} enter = 'false'> <div ref={TvRef}><TvSetup /></div> </Slide > : ''}
-                {current === 'TV' && (prevIndex < index) ? <Slide right duration= {700} enter = 'false'> <div ref={TvRef}><TvSetup /></div> </Slide > : ''} 
+                {current === 'Mobile' && (prevIndex >= index) ? <Slide left duration={700} enter='false'> <div ref={MobileRef}> <MobileSetup /></div> </Slide > : ''}
+                {current === 'Mobile' && (prevIndex < index) ? <Slide right duration={700} enter='false'> <div ref={MobileRef}> <MobileSetup /></div> </Slide > : ''}
 
-                {current === 'Box & Stick' && (prevIndex >= index) ?  <Slide left duration= {700} enter = 'false'> <div ref={BoxRef}><BoxSetup /></div> </Slide > : ''}
-                {current === 'Box & Stick' && (prevIndex < index) ? <Slide right duration= {700} enter = 'false'> <div ref={BoxRef}><BoxSetup /></div> </Slide > : ''}
+                {current === 'TV' && (prevIndex >= index) ? <Slide left duration={700} enter='false'> <div ref={TvRef}><TvSetup /></div> </Slide > : ''}
+                {current === 'TV' && (prevIndex < index) ? <Slide right duration={700} enter='false'> <div ref={TvRef}><TvSetup /></div> </Slide > : ''}
 
-                {current === 'Casting' && (prevIndex >= index) ?  <Slide left duration= {700} enter = 'false'> <div ref={CastingRef}><CastingSetup /></div>  </Slide > : ''}
-                {current === 'Casting' && (prevIndex < index) ?  <Slide right duration= {700} enter = 'false'> <div ref={CastingRef}><CastingSetup /></div>  </Slide > : ''}
+                {current === 'Box & Stick' && (prevIndex >= index) ? <Slide left duration={700} enter='false'> <div ref={BoxRef}><BoxSetup /></div> </Slide > : ''}
+                {current === 'Box & Stick' && (prevIndex < index) ? <Slide right duration={700} enter='false'> <div ref={BoxRef}><BoxSetup /></div> </Slide > : ''}
+
+                {current === 'Casting' && (prevIndex >= index) ? <Slide left duration={700} enter='false'> <div ref={CastingRef}><CastingSetup /></div>  </Slide > : ''}
+                {current === 'Casting' && (prevIndex < index) ? <Slide right duration={700} enter='false'> <div ref={CastingRef}><CastingSetup /></div>  </Slide > : ''}
             </div>
         </div >
     )
